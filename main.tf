36962814e12a75970467cff6c29f8e16a8f202fa
Pabl0FPP <pinedapablo6718@gmail.com>
2025-09-28 23:02:04 -0500

    feat: update storage_account_name in backend block

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "tfstate-rg"
-    storage_account_name  = "mytfstateaccount123"
+    storage_account_name  = "mytfstatepablo123"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
